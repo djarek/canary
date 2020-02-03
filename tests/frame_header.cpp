09555fd95c52df67c006eed22267faeaa3f08a8c
Damian Jarek <damian.jarek93@gmail.com>
2020-02-03 19:47:07 +0100

    Frame header tests
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

---
+++ tests/frame_header.cpp
@@ -0,0 +1,74 @@
+//
+// Copyright (c) 2020 Damian Jarek (damian.jarek93@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/djarek/canary
+//
+
+// Test if header is self-contained
+#include <canary/frame_header.hpp>
+
+#include <boost/core/lightweight_test.hpp>
+#include <linux/can.h>
+
+namespace
+{
+
+void
+test_layout()
+{
+    struct frame
+    {
+        canary::frame_header fh;
+        std::array<std::uint8_t, 8> data;
+    } f{};
+    ::can_frame cf;
+    static_assert(sizeof(f) == sizeof(cf));
+
+    f.fh.id(0x1EAD);
+    BOOST_TEST_EQ(f.fh.id(), 0x1EAD);
+
+    f.fh.extended_format(true);
+    f.fh.error(false);
+    f.fh.remote_transmission(false);
+    f.fh.payload_length(2);
+    std::memcpy(&cf, &f, sizeof(f));
+    BOOST_TEST_EQ(cf.can_id & CAN_EFF_MASK, 0x1EAD);
+    BOOST_TEST_EQ((cf.can_id & CAN_EFF_FLAG) >> 31, true);
+    BOOST_TEST_EQ((cf.can_id & CAN_RTR_FLAG) >> 30, false);
+    BOOST_TEST_EQ((cf.can_id & CAN_ERR_FLAG) >> 29, false);
+    BOOST_TEST_EQ(static_cast<int>(cf.can_dlc), 2);
+
+    f.fh.extended_format(false);
+    f.fh.error(false);
+    f.fh.remote_transmission(true);
+    f.fh.payload_length(3);
+    std::memcpy(&cf, &f, sizeof(f));
+    BOOST_TEST_EQ(cf.can_id & CAN_EFF_MASK, 0x1EAD);
+    BOOST_TEST_EQ((cf.can_id & CAN_EFF_FLAG) >> 31, false);
+    BOOST_TEST_EQ((cf.can_id & CAN_RTR_FLAG) >> 30, true);
+    BOOST_TEST_EQ((cf.can_id & CAN_ERR_FLAG) >> 29, false);
+    BOOST_TEST_EQ(static_cast<int>(cf.can_dlc), 3);
+
+    f.fh.extended_format(false);
+    f.fh.error(true);
+    f.fh.remote_transmission(false);
+    f.fh.payload_length(4);
+    std::memcpy(&cf, &f, sizeof(f));
+    BOOST_TEST_EQ(cf.can_id & CAN_EFF_MASK, 0x1EAD);
+    BOOST_TEST_EQ((cf.can_id & CAN_EFF_FLAG) >> 31, false);
+    BOOST_TEST_EQ((cf.can_id & CAN_RTR_FLAG) >> 30, false);
+    BOOST_TEST_EQ((cf.can_id & CAN_ERR_FLAG) >> 29, true);
+    BOOST_TEST_EQ(static_cast<int>(cf.can_dlc), 4);
+}
+
+} // namespace
+
+int
+main()
+{
+    test_layout();
+    return boost::report_errors();
+}
