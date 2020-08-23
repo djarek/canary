39c63463fb411b1b8dac6b9372dd3e2a35e953c6
Damian Jarek <damian.jarek93@gmail.com>
2020-08-23 03:19:33 +0200

    Frame filtering
    Provide bindings for frame filtering.
    
    Resolve: #14
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- include/canary/socket_options.hpp
+++ include/canary/socket_options.hpp
@@ -1,6 +1,12 @@
 #ifndef CANARY_SOCKET_OPTIONS_HPP
 #define CANARY_SOCKET_OPTIONS_HPP
 
+#include <canary/detail/config.hpp>
+#include <canary/filter.hpp>
+
+#ifdef CANARY_HAS_STD_SPAN
+#include <span>
+#endif // CANARY_HAS_STD_SPAN
 #include <cstdint>
 #include <linux/can/raw.h>
 
@@ -8,7 +14,7 @@ namespace canary
 {
 
 /// Enables the flexible data rate socket option.
-
+///
 /// Allows a raw CAN socket to receive Flexible Data rate frames. CAN FD frames
 /// support payloads up to 64 bytes, but standard data rate frames may still be
 /// received.
@@ -49,6 +55,106 @@ public:
 
 private:
     int value_;
+};
+
+/// Configures a raw CAN socket to use a disjunction of the filters provided to
+/// the constructor. A frame is accepted if it matches any provided filter.
+class filter_if_any
+{
+public:
+    /// Constructs the option object.
+    /// \param f a non-null pointer to an array of filter objects
+    /// \param n number of filters
+    explicit filter_if_any(filter const* f, std::size_t n)
+      : filters_{f}
+      , n_{n}
+    {
+    }
+
+#ifdef CANARY_HAS_STD_SPAN
+    explicit filter_if_any(std::span<filter const> fs)
+      : filter_if_any(fs.data(), fs.size())
+    {
+    }
+#endif // CANARY_HAS_STD_SPAN
+
+    template<class Protocol>
+    static int level(Protocol&& /*p*/)
+    {
+        return SOL_CAN_RAW;
+    }
+
+    template<class Protocol>
+    static int name(Protocol&& /*p*/)
+    {
+        return CAN_RAW_FILTER;
+    }
+
+    template<class Protocol>
+    void const* data(Protocol&& /*p*/) const
+    {
+        return filters_;
+    }
+
+    template<class Protocol>
+    std::size_t size(Protocol&& /*p*/) const
+    {
+        return n_ * sizeof(filter);
+    }
+
+private:
+    filter const* filters_;
+    std::size_t n_;
+};
+
+/// Configures a raw CAN socket to use a disjunction of the filters provided to
+/// the constructor. A frame is accepted if it matches all provided filters.
+class filter_if_all
+{
+public:
+    /// Constructs the option object.
+    /// \param f a non-null pointer to an array of filter objects
+    /// \param n number of filters
+    explicit filter_if_all(filter const* f, std::size_t n)
+      : filters_{f}
+      , n_{n}
+    {
+    }
+
+#ifdef CANARY_HAS_STD_SPAN
+    explicit filter_if_all(std::span<filter const> fs)
+      : filter_if_all(fs.data(), fs.size())
+    {
+    }
+#endif // CANARY_HAS_STD_SPAN
+
+    template<class Protocol>
+    static int level(Protocol&& /*p*/)
+    {
+        return SOL_CAN_RAW;
+    }
+
+    template<class Protocol>
+    static int name(Protocol&& /*p*/)
+    {
+        return CAN_RAW_JOIN_FILTERS;
+    }
+
+    template<class Protocol>
+    void const* data(Protocol&& /*p*/) const
+    {
+        return filters_;
+    }
+
+    template<class Protocol>
+    std::size_t size(Protocol&& /*p*/) const
+    {
+        return n_ * sizeof(filter);
+    }
+
+private:
+    filter const* filters_;
+    std::size_t n_;
 };
 
 } // namespace canary
--- tests/socket_options.cpp
+++ tests/socket_options.cpp
@@ -43,11 +43,75 @@ test_can_fd()
     sock2.receive(canary::net::buffer(&fr, sizeof(fr)));
 }
 
+void
+test_if_any_filter()
+{
+    canary::net::io_context ctx{1};
+    canary::raw::socket sock1{
+      ctx, canary::raw::endpoint{canary::get_interface_index("vcan0")}};
+    canary::raw::socket sock2{
+      ctx, canary::raw::endpoint{canary::get_interface_index("vcan0")}};
+
+    const canary::filter filters[] = {
+      canary::filter{}.id(0xFF).id_mask(~0u),
+      canary::filter{}.id(0xFFFF).id_mask(~0u).extended_format(true)};
+
+#ifdef CANARY_HAS_STD_SPAN
+    sock1.set_option(canary::filter_if_any{{filters}});
+#else  // CANARY_HAS_STD_SPAN
+    sock1.set_option(canary::filter_if_any{filters, 2});
+#endif // CANARY_HAS_STD_SPAN
+
+    struct frame
+    {
+        canary::frame_header header;
+        std::array<char, 8> data{};
+    } f;
+
+    f.header.payload_length(f.data.size());
+    f.header.id(0xFF);
+
+    sock2.send(canary::net::buffer(&f, sizeof(f)));
+    const auto n = sock1.receive(canary::net::buffer(&f, sizeof(f)));
+    BOOST_TEST_EQ(n, sizeof(f));
+    BOOST_TEST_EQ(f.header.id(), 0xFF);
+
+    f.header.id(0xFFFE);
+    f.header.extended_format(true);
+    sock2.send(canary::net::buffer(&f, sizeof(f)));
+    sock1.async_receive(canary::net::buffer(&f, sizeof(f)),
+                        [](canary::error_code ec, std::size_t n) {
+                            BOOST_TEST(ec);
+                            BOOST_TEST_EQ(n, 0);
+                        });
+    const auto run_count = ctx.run_for(std::chrono::milliseconds{100});
+    BOOST_TEST(run_count == 0);
+    sock1.cancel();
+}
+
+void
+test_if_filter_size_exceeded()
+{
+    canary::net::io_context ctx{1};
+    canary::raw::socket sock{
+      ctx, canary::raw::endpoint{canary::get_interface_index("vcan0")}};
+    std::vector<canary::filter> filters(CAN_RAW_FILTER_MAX * 2);
+
+    canary::error_code error;
+    sock.set_option(canary::filter_if_any{filters.data(), filters.size()}, error);
+    BOOST_TEST(error == std::errc::invalid_argument);
+
+    sock.set_option(canary::filter_if_all{filters.data(), filters.size()}, error);
+    BOOST_TEST(error == std::errc::invalid_argument);
+}
+
 } // namespace
 
 int
 main()
 {
     test_can_fd();
+    test_if_any_filter();
+    test_if_filter_size_exceeded();
     return boost::report_errors();
 }
