a286cb354c35a1794d2a24c8312a36b933fca73c
Damian Jarek <damian.jarek93@gmail.com>
2022-07-31 02:47:15 +0200

    Fix C++ version check
    Fixed a check for C++20 that incorrectly attempted to include span in
    C++17.
    
    Closes: #18
    
    Signed-off-by: Damian Jarek <damian.jarek93gmail.com>

--- include/canary/detail/config.hpp
+++ include/canary/detail/config.hpp
@@ -24,13 +24,13 @@
 #endif // CANARY_DECL
 #endif // CANARY_SEPARATE_COMPILATION
 
-#if __cplusplus >= 201703L
+#if __cplusplus >= 202002L
 #if __has_include(<span>)
 #ifndef CANARY_HAS_STD_SPAN
 #define CANARY_HAS_STD_SPAN
 #endif // CANARY_HAS_STD_SPAN
 #endif // __has_include(<span>)
-#endif // __cplusplus >= 201703L
+#endif // __cplusplus >= 202002L
 
 #ifdef CANARY_STANDALONE_ASIO
 
